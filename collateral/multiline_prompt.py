64060c2ebaa63089fbed1c0fbe3955e3c7caedef
Kirk Byers <ktbyers@twb-tech.com>
2020-10-09 07:39:04 -0700

    General collateral

---
+++ collateral/multiline_prompt.py
@@ -0,0 +1,30 @@
+import os
+from netmiko import ConnectHandler
+from getpass import getpass
+
+# Code so automated tests will run properly
+# Check for environment variable, if that fails, use getpass().
+password = os.getenv("NETMIKO_PASSWORD") if os.getenv("NETMIKO_PASSWORD") else getpass()
+
+my_device = {
+    "device_type": "cisco_ios",
+    "host": "cisco3.lasthop.io",
+    "username": "pyclass",
+    "password": password,
+    "session_log": "cisco3.out",
+}
+
+with ConnectHandler(**my_device) as net_connect:
+
+    filename = "cisco3-cfg-May-18-11-15-36.159-115"
+    cmd = f"del flash:/{filename}"
+    output = net_connect.send_command(
+        cmd, expect_string=r"Delete filename", strip_prompt=False, strip_command=False
+    )
+    output += net_connect.send_command(
+        "\n", expect_string=r"confirm", strip_prompt=False, strip_command=False
+    )
+    output += net_connect.send_command(
+        "y", expect_string=r"#", strip_prompt=False, strip_command=False
+    )
+    print(output)
---
+++ collateral/multiline_prompt_time.py
@@ -0,0 +1,30 @@
+import os
+from netmiko import ConnectHandler
+from getpass import getpass
+
+# Code so automated tests will run properly
+# Check for environment variable, if that fails, use getpass().
+password = os.getenv("NETMIKO_PASSWORD") if os.getenv("NETMIKO_PASSWORD") else getpass()
+
+my_device = {
+    "device_type": "cisco_ios",
+    "host": "cisco3.lasthop.io",
+    "username": "pyclass",
+    "password": password,
+    "session_log": "cisco3.out",
+}
+
+with ConnectHandler(**my_device) as net_connect:
+
+    filename = "cisco3-cfg-May-17-11-15-37.235-114"
+    cmd = f"del flash:/{filename}"
+    output = net_connect.send_command_timing(
+        cmd, strip_prompt=False, strip_command=False
+    )
+    output += net_connect.send_command_timing(
+        "\n", strip_prompt=False, strip_command=False
+    )
+    output += net_connect.send_command_timing(
+        "y", strip_prompt=False, strip_command=False
+    )
+    print(output)
