6b88d1ab16a705ddf367fea34499953b621e6f92
Kirk Byers <ktbyers@twb-tech.com>
2020-10-21 14:27:23 -0700

    Tests for class3

--- tests/test_class3.py
+++ tests/test_class3.py
@@ -25,32 +25,31 @@ def test_runner_collateral(test_case):
     assert "Traceback" not in std_out
 
 
-#def test_class2_ex1():
-#    base_path = "../class2/exercises/"
-#    cmd_list = ["python", "exercise1.py"]
-#    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
-#    assert return_code == 0
-#    assert std_err == ""
-#    assert std_out.count("10.220.88.1") == 4
-#
-#
-#def test_class2_ex2():
-#    base_path = "../class2/exercises/"
-#    cmd_list = ["python", "exercise2.py"]
-#    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
-#    assert return_code == 0
-#    assert std_err == ""
-#    assert "cisco3>" in std_out
-#    assert "cisco3#" in std_out
-#
-#
-#def test_class2_ex3():
-#    base_path = "../class2/exercises/"
-#    cmd_list = ["python", "exercise3.py"]
-#    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
-#    assert return_code == 0
-#    assert std_err == ""
-#    assert "cisco3>" in std_out
-#    assert "cisco3#" in std_out
-#    assert "cisco4>" in std_out
-#    assert "cisco4#" in std_out
+def test_class3_ex1():
+    base_path = "../class3/exercises/"
+    cmd_list = ["python", "exercise1.py"]
+    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
+    assert return_code == 0
+    assert std_err == ""
+    assert "VLAN ID: 7" in std_out
+    assert "VLAN0007" in std_out
+
+
+def test_class3_ex2():
+    base_path = "../class3/exercises/"
+    cmd_list = ["python", "exercise2.py"]
+    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
+    assert return_code == 0
+    assert std_err == ""
+    assert std_out.count("nxos2") == 4
+    assert std_out.count("10.0.2.16") == 4
+
+
+def test_class3_ex3():
+    base_path = "../class3/exercises/"
+    cmd_list = ["python", "exercise3.py"]
+    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
+    assert return_code == 0
+    assert std_err == ""
+    assert "VLAN ID: 7" in std_out
+    assert "VLAN0007" in std_out
