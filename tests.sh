37ab7df1c9fc577bba990b1bfae09e9974cc5038
Kirk Byers <ktbyers@twb-tech.com>
2021-02-16 10:56:43 -0800

    Updating test runner script

--- tests.sh
+++ tests.sh
@@ -8,10 +8,14 @@ echo "pylama ." \
 && black --check . \
 && echo "running pytest..." \
 && cd tests \
+&& py.test -x -s -v test_class1.py \
+&& py.test -x -s -v test_class2.py \
+&& py.test -x -s -v test_class3.py \
+&& py.test -x -s -v test_class4.py \
+&& py.test -x -s -v test_class5.py \
+&& py.test -x -s -v test_class6.py \
+&& py.test -x -s -v test_class7.py \
 \
 || RETURN_CODE=1
 
 exit $RETURN_CODE
-
-# && py.test -x -s -v test_class* \
-# && py.test -x -s -v test_bonus* \
