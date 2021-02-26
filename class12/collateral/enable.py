3efbfbc0402c63af9fa48bac37a7d319cb470f94
Kirk Byers <ktbyers@twb-tech.com>
2021-02-26 14:04:49 -0800

    Code on enable

--- class12/collateral/enable.py
+++ class12/collateral/enable.py
@@ -6,6 +6,14 @@ from netmiko import ConnectHandler
 password = os.getenv("NETMIKO_PASSWORD") if os.getenv("NETMIKO_PASSWORD") else getpass()
 
 
+def lower_privileges(net_connect):
+    net_connect.exit_enable_mode()
+    print("*" * 40)
+    print()
+    print(f"Current Prompt:\n{net_connect.find_prompt()}\n")
+    show_current_priv(net_connect)
+
+
 def show_current_priv(net_connect):
     output = net_connect.send_command("show priv")
     print(f"Current Privilege Level: {output}")
@@ -18,27 +26,32 @@ if __name__ == "__main__":
         "host": "cisco4.lasthop.io",
         "username": "pyclass",
         "password": password,
-        "secret": "foo",
+        "secret": password,
     }
-    
+
     with ConnectHandler(**my_device) as net_connect:
 
-        print() 
+        print()
         print(f"Current Prompt:\n{net_connect.find_prompt()}\n")
         show_current_priv(net_connect)
-    
-        # Lower privileges
-        net_connect.exit_enable_mode()
-        print("*" * 40)
-        print() 
-        print(f"Current Prompt:\n{net_connect.find_prompt()}\n")
-        show_current_priv(net_connect)
+
+        # Drop to privilege level1
+        lower_privileges(net_connect)
 
         # Use 'enable()' method to raise privileges. This will automatically handle the password
         # must have 'secret' argument set in the device dictionary.
-        net_connect.enable() 
+        net_connect.enable()
         print("*" * 40)
-        print() 
+        print()
         print(f"Current Prompt:\n{net_connect.find_prompt()}\n")
         show_current_priv(net_connect)
 
+        # Try again - go back to priv level1
+        lower_privileges(net_connect)
+
+        # Now call 'enable', but feed in a 'cmd'
+        net_connect.enable(cmd="enable 15")
+        print("*" * 40)
+        print()
+        print(f"Current Prompt:\n{net_connect.find_prompt()}\n")
+        show_current_priv(net_connect)
