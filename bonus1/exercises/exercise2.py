68bc65d7e9f67005f6c8e7f2fc8bf17dd17fdede
Kirk Byers <ktbyers@twb-tech.com>
2021-08-31 20:13:56 -0700

    Bonus1 exercises

---
+++ bonus1/exercises/exercise2.py
@@ -0,0 +1,32 @@
+import os
+from datetime import datetime
+from getpass import getpass
+from netmiko import ConnectHandler, ReadTimeout
+
+# Code so automated tests will run properly
+password = os.getenv("NETMIKO_PASSWORD") if os.getenv("NETMIKO_PASSWORD") else getpass    ()
+
+device = {
+    "host": "cisco3.lasthop.io",
+    "username": "pyclass",
+    "password": password,
+    "device_type": "cisco_xe",
+}
+
+try:
+    ssh_conn = ConnectHandler(**device)
+    start = datetime.now()
+    show_tech = ssh_conn.send_command("show tech-support", read_timeout=120)
+    print("\nCommand Succeeded.\n")
+except ReadTimeout:
+    print("\nProgram failed with ReadTimeout Exception.\n")
+finally:
+    end = datetime.now()
+
+try:
+    ssh_conn.disconnect()
+except:
+    pass
+
+print(f"Execution time: {end - start}")
+print("\n")
---
+++ bonus1/exercises/exercise3.py
@@ -0,0 +1,36 @@
+from getpass import getpass
+from datetime import datetime
+from netmiko import ConnectHandler, ReadTimeout
+
+device = {
+    "host": "cisco3.lasthop.io",
+    "username": "pyclass",
+    "password": getpass(),
+    "device_type": "cisco_xe",
+    "session_log": "traceroute.out",
+}
+
+
+command = "show tech-support"
+ssh_conn = ConnectHandler(**device)
+
+try:
+    # Timeout in 15 seconds
+    start_time = datetime.now()
+    output = ssh_conn.send_command_timing(command, last_read=10, read_timeout=15)
+except ReadTimeout:
+    end_time = datetime.now()
+    print(f"Timed out: {end_time - start_time}")
+
+# Gather the entire output
+start_time = datetime.now()
+output = ssh_conn.send_command_timing(command, last_read=10, read_timeout=120, strip_prompt=False)
+end_time = datetime.now()
+
+print("-" * 80)
+print(output)
+print("-" * 80)
+print("\n\n")
+print(f"Exec time: {end_time - start_time}")
+
+ssh_conn.disconnect()
