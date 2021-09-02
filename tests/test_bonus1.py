d00e6ca55c9a72b9467b81400df0c8a24055dd5f
Kirk Byers <ktbyers@twb-tech.com>
2021-09-01 20:09:29 -0700

    Black

--- tests/test_bonus1.py
+++ tests/test_bonus1.py
@@ -6,14 +6,14 @@ from utilities import subprocess_runner
 
 
 TEST_COLLATERAL = [
-#    "../bonus1/collateral/send_command/traceroute_working.py",
-#    "../bonus1/collateral/send_command_timing/traceroute_working.py",
+    #    "../bonus1/collateral/send_command/traceroute_working.py",
+    #    "../bonus1/collateral/send_command_timing/traceroute_working.py",
 ]
 
 TEST_COLLATERAL_EXC = [
-#    "../bonus1/collateral/send_command/traceroute_timeout.py",
-#    "../bonus1/collateral/send_command/traceroute_long.py",
-#    "../bonus1/collateral/send_command_timing/traceroute_timeout.py",
+    #    "../bonus1/collateral/send_command/traceroute_timeout.py",
+    #    "../bonus1/collateral/send_command/traceroute_long.py",
+    #    "../bonus1/collateral/send_command_timing/traceroute_timeout.py",
 ]
 
 
@@ -53,6 +53,7 @@ def test_bonus1_ex1():
     assert "Execution time" in std_out
     # Let show tech-support complete
     time.sleep(120)
+
 
 def test_bonus1_ex2():
     base_path = "../bonus1/exercises/"
