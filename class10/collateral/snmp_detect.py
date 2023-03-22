71c03289d1af06f0f2982deb0b7038f127cb63fc
Kirk Byers <ktbyers@twb-tech.com>
2023-03-22 11:33:11 -0700

    Fixing SNMP autodetect hostname issue

--- class10/collateral/snmp_detect.py
+++ class10/collateral/snmp_detect.py
@@ -6,8 +6,10 @@ snmp_key = os.getenv("SNMP_COMMUNITY")
 if not snmp_key:
     snmp_key = getpass("Enter SNMP community: ")
 
+# Had to change from a hostname to an IP address due to a Netmiko bug
+# in the SNMP autodetect code.
 my_snmp = SNMPDetect(
-    "cisco1.lasthop.io",
+    "184.105.247.70",
     snmp_version="v3",
     user="pysnmp",
     auth_key=snmp_key,
